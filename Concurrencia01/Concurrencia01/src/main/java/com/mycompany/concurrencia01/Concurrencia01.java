b5c137c3bbdbdda928b8a4f76f87ec422d5529be
pnh0002 <pnietohurtado03@gmail.com>
2025-09-15 16:07:20 +0200

    First commit

---
+++ Concurrencia01/Concurrencia01/src/main/java/com/mycompany/concurrencia01/Concurrencia01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.concurrencia01;
+
+/**
+ *
+ * @author pablo
+ */
+public class Concurrencia01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
