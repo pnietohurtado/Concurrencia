2fd9a8d9c1cfbfadd15038d17e1b2c4721176ece
pnh0002 <pnietohurtado03@gmail.com>
2025-09-24 17:11:01 +0200

    Apuntes de ReadWriteLock

--- Concurrencia01/Concurrencia01/src/main/java/com/mycompany/concurrencia01/Concurrencia12.java
+++ Concurrencia01/Concurrencia01/src/main/java/com/mycompany/concurrencia01/Concurrencia12.java
@@ -48,11 +48,14 @@ public class Concurrencia12 {
     }
     
     public static void main(String[] args) {
-        
         Thread t1 = new Thread(() -> abrirPuerta()); 
+        Thread t3 = new Thread(() -> abrirPuerta()); 
         Thread t2 = new Thread(() -> sonar()); 
+        Thread t4 = new Thread(() -> sonar()); 
         t1.start();
+        t3.start(); 
         t2.start();
+        t4.start();
         
         
     }
