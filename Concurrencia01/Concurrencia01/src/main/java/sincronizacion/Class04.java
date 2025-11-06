f63626dadc84d106fc93d5d5307e76646ae7fcd0
pnietohurtado <pnietohurtado03@gmail.com>
2025-11-06 18:02:45 +0100

    Falta hacer lo de los filosofos

---
+++ Concurrencia01/Concurrencia01/src/main/java/sincronizacion/Class04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sincronizacion;
+
+/**
+ *
+ * @author pablo
+ */
+public class Class04 {
+    public static void main(String[] args) {
+        
+    }
+   
+}
